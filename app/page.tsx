c45fbddb50ce9adb28f0526d1256f51117eddd3a
Md. Touhidur Rahman <touhidurrr@pm.me>
2024-11-19 16:16:04 +0600

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,46 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function Home() {
+  return (
+    <div className="min-h-full min-w-full flex flex-col justify-center items-center">
+      <h1>BUBT Enhanced Schedule Tracker</h1>
+
+      <div className="flex">
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select Building" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Department</SelectLabel>
+              <SelectItem value="b2">B2</SelectItem>
+              <SelectItem value="b3">B3</SelectItem>
+              <SelectItem value="b4">B4</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select Room" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Intake</SelectLabel>
+              <SelectItem value="room-313">Room 313</SelectItem>
+              <SelectItem value="room-314">Room 314</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+    </div>
+  );
+}

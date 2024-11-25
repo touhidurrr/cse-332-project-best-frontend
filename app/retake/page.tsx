58cc9723ac6410ece2d95bed394c1ec1a2c2b7bb
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-11-25 16:29:02 +0600

    update retake page

--- app/retake/page.tsx
+++ app/retake/page.tsx
@@ -1,17 +1,10 @@
 "use client";
 
-import { useEffect } from "react";
-
 export default function Retake() {
-  useEffect(() => {
-    window?.document
-      ?.getElementsByClassName("main")[0]
-      ?.classList?.add("flex-grow");
-  }, []);
   return (
     <iframe
       src="https://retake-section-finder.pages.dev/"
-      className="h-full w-full"
+      className="min-h-full min-w-full"
     />
   );
 }

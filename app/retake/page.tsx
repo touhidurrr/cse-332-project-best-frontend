6e04592ab2c6cbbae37284c77a9f92e3e314b4f9
Md. Touhidur Rahman <touhidurrr@pm.me>
2024-11-25 15:42:11 +0600

    fix issues

--- app/retake/page.tsx
+++ app/retake/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { useEffect } from "react";
+
 export default function Retake() {
-  window.document.getElementsByClassName("main")[0]?.classList.add("flex-grow");
+  useEffect(() => {
+    window?.document
+      ?.getElementsByClassName("main")[0]
+      ?.classList?.add("flex-grow");
+  }, []);
   return (
     <iframe
       src="https://retake-section-finder.pages.dev/"
